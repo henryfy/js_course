65e3e035de234a9bf46d6c7f419a90781f2b4fe9
Henry <henry.franconieri@gmail.com>
2020-12-17 10:04:52 +1100

    create (folder - scripts --> calc.js | controller --> CalcController.js)

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,23 @@
+class CalcController {
+
+    constructor(){
+
+        this._displayCalc = "0";
+        this._dataAtual = "0";
+
+    }
+
+    get displayCalc(){
+
+        return this._displayCalc;
+    }
+
+    set displayCalc(value){
+
+        this._displayCalc = value;
+    }
+
+
+
+
+}
